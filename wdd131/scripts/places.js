1d1b870144f2648b2b34fd0a7695914577cc4b38
Gabriel Perez Sanchez <kalel.perez.sanchez@gmail.com>
2025-09-20 19:25:37 -0500

    Delete wdd131 directory

--- wdd131/scripts/places.js
+++
@@ -1,5 +0,0 @@
-// scripts/places.js
-document.addEventListener("DOMContentLoaded", () => {
-  const lastModified = document.lastModified;
-  document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
-});
