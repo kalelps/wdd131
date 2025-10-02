2fbaa50f701364b0d93caa88618adad8f07759f2
Gabriel Perez Sanchez <kalel.perez.sanchez@gmail.com>
2025-10-02 13:39:41 -0500

    Add files via upload

--- scripts/review.js
+++ scripts/review.js
@@ -1,9 +1,7 @@
-
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
 let count = Number(localStorage.getItem("reviewCount")) || 0;
 count++;
 localStorage.setItem("reviewCount", count);
-
 document.getElementById("reviewCount").textContent = count;
